738f786f97ec0e092ae76d983f4660d0c9ee271f
vishumane <47922687+vishumane@users.noreply.github.com>
2021-09-06 16:06:35 +0530

    use react redux for api call
    use react redux for api call

--- api_integration/src/App.js
+++ api_integration/src/App.js
@@ -3,12 +3,19 @@ import './App.css';
 import DisplayData from'./component/getapi/DisplayData';
 import Form from'./component/postapi/Form';
 import ToastMessage from'./component/toastMessage/ToastMessage';
+import { Provider } from 'react-redux'
+import store from './component/redux/store';
+import UsersContainer from'./component/UsersContainer';
 const App=()=> {
   return (
     <div className="App">
      <DisplayData/>
      <ToastMessage/>
      <Form/>
+
+     <Provider store={store}>
+        <UsersContainer />
+        </Provider>
     </div>
   );
 }
